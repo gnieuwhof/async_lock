26cc761852b37d55fc3de44e9d580ba6e54f0c89
Guido Nieuwhof <g.nieuwhofg@viadata.nl>
2021-10-24 03:12:59 +0200

    Added VS solution.

---
+++ AsyncLock/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AsyncLock
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
