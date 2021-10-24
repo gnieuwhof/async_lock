bb296b6aed48533a38a32be19096dafe14bdeff6
Guido Nieuwhof <g.nieuwhofg@viadata.nl>
2021-10-24 04:06:28 +0200

    Added AsyncLock file/logic.

---
+++ AsyncLock/AsyncLock.cs
@@ -0,0 +1,60 @@
+﻿namespace AsyncLock
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /*    
+    // Note: Unlike the lock keyword this AsyncLock cannot be nested!
+
+    // Usage:
+    public class Foo
+    {
+        private readonly SemaphoreSlim semaphore =
+            AsyncLock.CreateSemaphore();
+
+        private async Task Bar()
+        {
+            using (await AsyncLock.Enter(semaphore))
+            {
+                // Your code...
+            }
+        }
+    }
+    */
+    public sealed class AsyncLock : IDisposable
+    {
+        private readonly SemaphoreSlim semaphore;
+
+
+        private AsyncLock(SemaphoreSlim semaphore)
+        {
+            this.semaphore = semaphore;
+        }
+
+
+        public static SemaphoreSlim CreateSemaphore()
+        {
+            var semaphore = new SemaphoreSlim(1, 1);
+
+            return semaphore;
+        }
+
+        public static async Task<AsyncLock> Enter(SemaphoreSlim semaphore)
+        {
+            _ = semaphore ??
+                throw new ArgumentNullException(nameof(semaphore));
+
+            await semaphore.WaitAsync();
+
+            var asyncLock = new AsyncLock(semaphore);
+
+            return asyncLock;
+        }
+
+        public void Dispose()
+        {
+            this.semaphore.Release();
+        }
+    }
+}
--- AsyncLock/Program.cs
+++ AsyncLock/Program.cs
@@ -1,12 +1,40 @@
 ﻿namespace AsyncLock
 {
     using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     public class Program
     {
+        private static readonly SemaphoreSlim semaphore =
+            AsyncLock.CreateSemaphore();
+
+        private static Random rand = new Random(Guid.NewGuid().GetHashCode());
+        private static int counter = 0;
+
+
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var tasks = new List<Task>();
+
+            for(int i = 0; i < 10; ++i)
+            {
+                tasks.Add(Test());
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private static async Task Test()
+        {
+            using (await AsyncLock.Enter(semaphore))
+            {
+                int ms = rand.Next(0, 100);
+                await Task.Delay(ms);
+                ++counter;
+                Console.WriteLine($"{counter}: Hello, World!");
+            }
         }
     }
 }
