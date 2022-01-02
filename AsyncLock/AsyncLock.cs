6cef36e1792f25fd9d03915e00ccd9d5f8d9429c
Guido Nieuwhof <guido.nieuwhof@gmail.com>
2022-01-02 03:23:39 +0100

    Added readme file.

--- AsyncLock/AsyncLock.cs
+++ AsyncLock/AsyncLock.cs
@@ -22,6 +22,7 @@
         }
     }
     */
+
     public sealed class AsyncLock : IDisposable
     {
         private readonly SemaphoreSlim semaphore;
---
+++ README.md
@@ -0,0 +1,8 @@
+# AsyncLock
+
+Class to use one level deep async locking.
+Note: this (ab)uses the C# using statement.
+
+## License
+
+[MIT](LICENSE)
